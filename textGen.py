ffd1d033e3224a6e7df5d641394e836313f0f064
Vitalii H <vitalii_h@gmail.com>
2025-09-25 13:05:51 +0200

    Python Code

---
+++ textGen.py
@@ -0,0 +1,9 @@
+# start
+name = "Vitalii"
+country = "Ukraine" 
+age = "48"
+result = ""
+
+result = "Hello, my Name is "+name+" and I am "+age+" years old and I come from "+country+"."
+print(result)
+# end
