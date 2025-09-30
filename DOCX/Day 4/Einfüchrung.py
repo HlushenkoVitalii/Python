ef7c7a4d36b82c935929e6fd1a2d9604515cf6c0
Vitalii H <vitalii_h@gmail.com>
2025-09-30 10:00:30 +0200

    Day 4

---
+++ DOCX/Day 4/Einfüchrung.py
@@ -0,0 +1,12 @@
+#Deklaration
+
+umschueler = "Hallo Welt!" #Python macht automatisch Initialisiet variabel. In diese Fall hat Python die Variable umschueler initialisiert als string
+
+a = 10
+b = 5
+c = a/b
+print(c)
+
+print(10/3)
+print(f"{a/b}")
+print("Das Ergebnis ist: ", a/b)
