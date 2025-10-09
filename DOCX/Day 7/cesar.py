513a7e6b6b7cfa6ea1a596bb97ef69e2a8637535
Vitalii H <vitalii_h@gmail.com>
2025-10-09 12:13:30 +0200

    logik encrypt

--- DOCX/Day 7/cesar.py
+++ DOCX/Day 7/cesar.py
@@ -1,9 +1,23 @@
 # implementation Cesar Encryption
 
+
+ALPHABET = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+]
+
+
 #this function do encyption
 def encrypt()->str:
     klar_text = input("\nplease enter Text to be encrypted:")
     key = input("please enter encryption Key:")
+    for charecter in klar_text:
+        print(f"\n {charecter}")
+        print(f" index position in kunde text for Alphabet is: " + str(ALPHABET.index(charecter)))
+
+        new_charected_index = ALPHABET.index(charecter)+int(key)
+        encryptedChar = ALPHABET[new_charected_index]
+        print(f"it is encrypted to " + encryptedChar)
     return "it is encrypted " + klar_text + " and " + str(key)
 
 def decode()->str:
